f6cea870ccdc0e19d862ce676f2d19f33eaa777b
Pedram Alizadeh <83alizadeh13ped@gmail.com>
2024-10-28 20:16:34 +0330

    successor function created, attribute __obstacle_cycle added to PlayGround, several minor modification made and minute functions added to the Playground

--- PlayGround.py
+++ PlayGround.py
@@ -9,7 +9,7 @@ class PlayGround:
     def __init__(self, player: list, obstacles: list, pucks: list, goals: list):
         """
         Initializes:\n
-        _player, _obstacles, _pucks, _goals
+        __player, __obstacles, __pucks, __goals, __obstacle_cycle
 
         :param player: player[0] the x-coordinate and player[1] the y-coordinate
         :param obstacles: a list of lists that coordinates are stored in similar to the player
@@ -20,6 +20,7 @@ class PlayGround:
         self.__obstacles = obstacles
         self.__pucks = pucks
         self.__goals = goals
+        self.__obstacle_cycle = 0
 
     def __eq__(self, other) -> bool:
         """Checks if the state of two playground objects are the same in a search algorithm
@@ -82,6 +83,13 @@ class PlayGround:
             cls._num_rows = len(cost_matrix)
             cls._num_columns = len(cost_matrix[0])
 
+    @classmethod
+    def is_index_within_range(cls, position : list) -> bool:
+        if 0 <= position[0] < cls._num_columns:
+            if 0 <= position[1] < cls._num_rows:
+                return True
+        return False
+
     def is_playground_valid(self) -> bool:
         """
         Checks the collisions of player and the pucks with obstacles or themselves
@@ -103,6 +111,39 @@ class PlayGround:
             obstacles_and_pucks.append(player_or_puck)
 
         return True
+
+    def successor_func(self) -> list:
+        """
+        Creates all possible successors of the current playground state
+        :return: a list of valid future states
+        """
+
+        successor_states = []
+        obstacle_cycle_direction = {0: (0, 1), 1: (-1, 0), 2: (0, -1), 3: (1, 0)}
+        #each cycle refers to all the obstacles position relative to the 2 by 2 square they rotate counterclockwise
+        directions = [(0, 1), (0, -1), (0, -1), (0, 1)] #UP, DOWN, LEFT, RIGHT
+        for direction in directions:
+            new_player_position = self.__player
+            for i in range(0, 2):
+                new_player_position[i] += direction[i]
+            if not PlayGround.is_index_within_range(new_player_position):
+                continue
+            new_pucks_positions = []
+            for puck_position in self.__pucks:
+                if new_player_position == puck_position:
+                    for i in range(0, 2):
+                        puck_position[i] += direction[i]
+                    if not PlayGround.is_index_within_range(puck_position):
+                        continue
+                new_pucks_positions.append(puck_position)
+            new_obstacle_positions = []
+            obstacle_direction = obstacle_cycle_direction.get(self.__obstacle_cycle)
+            for obstacle in self.__obstacles:
+                new_obstacle_positions.append([obstacle[0] + obstacle_direction[0], obstacle[1] + obstacle_direction[1]])
+            possible_future_state = PlayGround(new_player_position, new_obstacle_positions, new_pucks_positions, self.goals)
+            if possible_future_state.is_playground_valid():
+                successor_states.append(possible_future_state)
+        return successor_states
 
 #test case here!
 if __name__ == '__main__':

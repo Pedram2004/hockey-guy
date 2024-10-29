028921d2dccd0a65d9616872f17e8f6ded29376a
Pedram Alizadeh <83alizadeh13ped@gmail.com>
2024-10-29 10:41:17 +0330

    PlayGround class debuged, problems regarding memory aliasing in lists avoided by changing to tuples, methods adjusted relatively

--- PlayGround.py
+++ PlayGround.py
@@ -2,17 +2,17 @@ import copy
 
 
 class PlayGround:
-    _cost_matrix = []
+    _cost_matrix = tuple()
     _num_rows = -1
     _num_columns = -1
 
-    def __init__(self, player: list, obstacles: list, pucks: list, goals: list, obstacle_cycle=0):
+    def __init__(self, player: tuple, obstacles: list, pucks: list, goals: list, obstacle_cycle=0):
         """
         Initializes:\n
         __player, __obstacles, __pucks, __goals, __obstacle_cycle
 
-        :param player: player[0] the x-coordinate and player[1] the y-coordinate
-        :param obstacles: a list of lists that coordinates are stored in similar to the player
+        :param player: a tuple with player[0] the x-coordinate and player[1] the y-coordinate
+        :param obstacles: a list of tuples that in each tuple coordinates are stored in similar to the player
         :param pucks: similar to obstacles
         :param goals: similar to obstacles
         """
@@ -47,50 +47,51 @@ class PlayGround:
 
     @player.setter
     def player(self, player):
-        self.__player = copy.copy(player)
+        self.__player = tuple(player)
+
 
     @property
     def obstacles(self):
-        return copy.copy(self.__obstacles)
+        return self.__obstacles
 
     @obstacles.setter
     def obstacles(self, obstacles):
-        self.__obstacles = copy.copy(obstacles)
+        self.__obstacles = tuple(obstacles)
 
     @property
     def pucks(self):
-        return copy.copy(self.__pucks)
+        return self.__pucks
 
     @pucks.setter
     def pucks(self, pucks):
-        self.__pucks = copy.copy(pucks)
+        self.__pucks = tuple(pucks)
 
     @property
     def goals(self):
-        return copy.copy(self.__goals)
+        return self.__goals
 
     @goals.setter
     def goals(self, goals):
-        self.__goals = copy.copy(goals)
+        self.__goals = tuple(goals)
 
     @property
     def cost_matrix(self):
-        return copy.copy(self._cost_matrix)
+        return self._cost_matrix
 
     @classmethod
     def set_class_vars(cls, cost_matrix: list) -> None:
         """
         Initializes:\n
         the class variables of _cost_matrix, _num_rows and _num_columns
-        :param cost_matrix: a list of lists with n lists that contain m elements (n*m blocks the same size the playground), specifies the cost of each move of the player to any specific point
+        :param cost_matrix: a tuple of tuples with n tuples that contain m elements (n*m blocks the same size the playground), specifies the cost of each move of the player to any specific point
         """
         if cls._num_rows == -1 and cls._num_columns == -1:
-            cls._cost_matrix = copy.copy(cost_matrix)
+            cls._cost_matrix = tuple(cost_matrix)
             cls._num_rows = len(cost_matrix)
             cls._num_columns = len(cost_matrix[0])
 
     @classmethod
-    def is_index_within_range(cls, position: list) -> bool:
+    def is_index_within_range(cls, position) -> bool:
         if 0 <= position[0] < cls._num_columns:
             if 0 <= position[1] < cls._num_rows:
                 return True
@@ -126,25 +127,25 @@ class PlayGround:
         # each cycle refers to all the obstacles position relative to the 2 by 2 square they rotate counterclockwise
         directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]  # UP, DOWN, LEFT, RIGHT
         for direction in directions:
-            new_player_position = self.player
+            new_player_position = list(self.player)
             for i in range(0, 2):
                 new_player_position[i] += direction[i]
             if not PlayGround.is_index_within_range(new_player_position):
                 continue
             new_pucks_positions = []
-            for puck_position in self.__pucks:
+            for puck_position in list(self.__pucks):
+                puck_position = list(puck_position)
                 if new_player_position == puck_position:
                     for j in range(0, 2):
                         puck_position[j] += direction[j]
                     if not PlayGround.is_index_within_range(puck_position):
                         continue
-                new_pucks_positions.append(puck_position)
+                new_pucks_positions.append(tuple(puck_position))
             new_obstacle_positions = []
             obstacle_direction = obstacle_cycle_direction.get(self.__obstacle_cycle)
             for obstacle in self.__obstacles:
-                new_obstacle_positions.append(
-                    [obstacle[0] + obstacle_direction[0], obstacle[1] + obstacle_direction[1]])
-            possible_future_state = PlayGround(new_player_position, new_obstacle_positions, new_pucks_positions,
+                new_obstacle_positions.append((obstacle[0] + obstacle_direction[0], obstacle[1] + obstacle_direction[1]))
+            possible_future_state = PlayGround(tuple(new_player_position), new_obstacle_positions, new_pucks_positions,
                                                self.goals, (self.__obstacle_cycle + 1) % 4)
             if possible_future_state.is_playground_valid():
                 successor_states.append(possible_future_state)
@@ -154,13 +155,14 @@ class PlayGround:
 # test cases here!
 if __name__ == '__main__':
     PlayGround.set_class_vars([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-    p1 = PlayGround([0, 0], [[1, 1], [2, 1]], [[1, 0], [2, 3]], [[3, 2]])
-    p2 = PlayGround([0, 0], [[1, 1], [2, 1]], [[1, 0], [0, 1]], [[3, 2]])
-    p3 = PlayGround([0, 1], [[0, 1], [1, 1]], [[2, 0], [0, 2]], [[3, 2]])
+    p1 = PlayGround((0, 0), [(1, 1), (2, 1)], [(1, 0), (2, 3)], [[3, 2]])
+    p2 = PlayGround((0, 0), [(1, 1), (2, 1)], [(1, 0)], [(3, 2)])
+    p3 = PlayGround((0, 1), [(0, 1), (1, 1)], [(2, 0), (0, 2)], [(3, 2)])
     playgrounds = [p1, p2]
     print(p3.is_playground_valid())
     p2.successor_func()
     for playground in playgrounds:
         for future in playground.successor_func():
             print(future)
+            print()
         print()

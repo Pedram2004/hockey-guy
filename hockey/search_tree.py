e6186e53cb687888e37e34a4e4bdc30d57362061
Magnesium <mehdighorashi83@gmail.com>
2024-11-03 23:02:45 +0330

    added breadth first search algorithm to STree class

--- hockey/search_tree.py
+++ hockey/search_tree.py
@@ -1,4 +1,4 @@
-from .search_tree_node import Node
+from search_tree_node import Node
 from collections import deque
 import heapq
 
@@ -8,4 +8,35 @@ class STree:
     def __init__(self, root: Node):
         self.__root = root
 
+    def breadth_first_search(self) -> Node:
+        queue = deque([self.__root])
+        visited = set()
+        visited.add(self.__root)
+        while queue:
+            current_node = queue.popleft()
+            print("\n-------------------------\ncurrent_node:\n", current_node, "\n-------------------------")
+            current_node.create_children()
+            for child in current_node.children:
+                if child.is_final:
+                    return child
+                elif child not in visited:
+                    print("child:\n", child)
+                    visited.add(child)
+                    queue.append(child)
+        return None
 
+if __name__ == "__main__":
+    from playground import PlayGround
+    playground = PlayGround(
+        player=(0, 0),
+        obstacles=[(2, 1), (2, 2)],
+        pucks=[((3, 1), False), ((3, 3), False)],
+        goals=[(4, 1), (1, 4)]
+    )
+    playground.set_class_vars(cost_matrix=((1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1)))
+    root = Node(state=playground)
+    tree = STree(root=root)
+    result = tree.bfs()
+    print("\n\n-------------------------\nresult:\n", result, "\n-------------------------")
+
+    print(result.get_full_direction())

c5f0e0c2ff3607d2864e5b5afb47ef9bb39f93fd
Pedram Alizadeh <83alizadeh13ped@gmail.com>
2024-11-05 00:40:30 +0330

    changed uninformed_search parameters and fixed it added IDS to be implemented later

--- hockey/search_tree.py
+++ hockey/search_tree.py
@@ -7,39 +7,36 @@ class STree:
     def __init__(self, root: Node):
         self.__root = root
 
-    def uninformed_search(self, priority_queue_class, pop_func, append_func) -> Node | None:
+    def uninformed_search(self, priority_queue, pop_func, append_func) -> Node | None:
 
-        class PriorityQueue(priority_queue_class):
-            def __init__(self, assigned_iter):
-                super().__init__(assigned_iter)
-
-            def pop_method(self):
-                return pop_func(self)
-
-            def append_method(self, item):
-                append_func(self, item)
-
-        priority_queue = PriorityQueue([self.__root])
         visited_nodes = {self.__root, }
         while priority_queue:
-            current_node = priority_queue.pop_method()
+            current_node = pop_func(priority_queue)
             current_node.create_children()
+
             for child_node in current_node.children:
                 if child_node.is_final:
                     return child_node
                 elif child_node not in visited_nodes:
                     visited_nodes.add(child_node)
-                    priority_queue.append_method(child_node)
+                    append_func(priority_queue, child_node)
+
         return None
 
     def breadth_first_search(self):
-        return self.uninformed_search(deque, deque.popleft, deque.append)
+        return self.uninformed_search(deque([self.__root]), deque.popleft, deque.append)
 
     def depth_first_search(self):
-        return self.uninformed_search(deque, deque.pop, deque.append)
+        return self.uninformed_search(deque([self.__root]), deque.pop, deque.append)
 
     def uniform_cost_search(self):
-        return self.uninformed_search(heapq, heapq.heappush, heapq.heappop)
+        return self.uninformed_search([self.__root], heapq.heappop, heapq.heappush)
+
+    def iterative_deepening_search(self):
+        max_depth = 1000
+        for i in range(max_depth):
+            pass
+        # depth_first_search here with changes
 
 
 if __name__ == "__main__":
@@ -52,10 +49,17 @@ if __name__ == "__main__":
         goals=[(4, 1), (1, 4)]
     )
     playground.set_class_vars(
-        cost_matrix=((1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1)))
+        cost_matrix=((1, 1, 1, 1, 1),
+                     (1, 1, 1, 1, 1),
+                     (1, 1, 1, 1, 1),
+                     (1, 1, 1, 1, 1),
+                     (1, 1, 1, 1, 1)))
     node_root = Node(state=playground)
     tree = STree(root=node_root)
-    result = tree.depth_first_search()
+    result = tree.uniform_cost_search()
+    print("\n\n-------------------------\nresult:\n", result, "\n-------------------------")
+    print(result.get_full_direction())
+    result = tree.breadth_first_search()
     print("\n\n-------------------------\nresult:\n", result, "\n-------------------------")
 
     print(result.get_full_direction())

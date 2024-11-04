18908c52ea93400e4ee3d7693d3cac22215fb350
Pedram Alizadeh <83alizadeh13ped@gmail.com>
2024-11-04 11:52:43 +0330

    uninformed_search method created to be used as an intermediary bfs, dfs and ucs adjusted accordingly, might need debugging

--- hockey/search_tree.py
+++ hockey/search_tree.py
@@ -3,40 +3,59 @@ from collections import deque
 import heapq
 
 
-
 class STree:
     def __init__(self, root: Node):
         self.__root = root
 
-    def breadth_first_search(self) -> Node:
-        queue = deque([self.__root])
-        visited = set()
-        visited.add(self.__root)
-        while queue:
-            current_node = queue.popleft()
-            print("\n-------------------------\ncurrent_node:\n", current_node, "\n-------------------------")
+    def uninformed_search(self, priority_queue_class, pop_func, append_func) -> Node | None:
+
+        class PriorityQueue(priority_queue_class):
+            def __init__(self, assigned_iter):
+                super().__init__(assigned_iter)
+
+            def pop_method(self):
+                return pop_func(self)
+
+            def append_method(self, item):
+                append_func(self, item)
+
+        priority_queue = PriorityQueue([self.__root])
+        visited_nodes = {self.__root, }
+        while priority_queue:
+            current_node = priority_queue.pop_method()
             current_node.create_children()
-            for child in current_node.children:
-                if child.is_final:
-                    return child
-                elif child not in visited:
-                    print("child:\n", child)
-                    visited.add(child)
-                    queue.append(child)
+            for child_node in current_node.children:
+                if child_node.is_final:
+                    return child_node
+                elif child_node not in visited_nodes:
+                    visited_nodes.add(child_node)
+                    priority_queue.append_method(child_node)
         return None
+
+    def breadth_first_search(self):
+        return self.uninformed_search(deque, deque.popleft, deque.append)
+
+    def depth_first_search(self):
+        return self.uninformed_search(deque, deque.pop, deque.append)
+
+    def uniform_cost_search(self):
+        return self.uninformed_search(heapq, heapq.heappush, heapq.heappop)
+
 
 if __name__ == "__main__":
     from playground import PlayGround
+
     playground = PlayGround(
         player=(0, 0),
         obstacles=[(2, 1), (2, 2)],
         pucks=[((3, 1), False), ((3, 3), False)],
         goals=[(4, 1), (1, 4)]
     )
-    playground.set_class_vars(cost_matrix=((1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1)))
-    root = Node(state=playground)
-    tree = STree(root=root)
-    result = tree.bfs()
+    playground.set_class_vars(
+        cost_matrix=((1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1)))
+    node_root = Node(state=playground)
+    tree = STree(root=node_root)
+    result = tree.depth_first_search()
     print("\n\n-------------------------\nresult:\n", result, "\n-------------------------")
 
     print(result.get_full_direction())

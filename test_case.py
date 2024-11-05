d838eb30afaa5bee99b3033a5800bf91cccbf254
Pedram Alizadeh <83alizadeh13ped@gmail.com>
2024-11-05 12:58:45 +0330

    created test_case.py to include some test cases in a dictionary format still not completed

---
+++ test_case.py
@@ -0,0 +1,50 @@
+test1 = {
+    "difficulty": "MED",
+    "player": (0, 0),
+    "pucks": [((3, 1), False), ((3, 3), False)],
+    "obstacles": [(2, 1), (2, 2)],
+    "goals": [(4, 1), (1, 4)],
+    "cost_matrix": ((1, 1, 1, 1, 1),
+                    (1, 1, 1, 1, 1),
+                    (1, 1, 1, 1, 1),
+                    (1, 1, 1, 1, 1),
+                    (1, 1, 1, 1, 1))
+}
+
+test2 = {
+    "difficulty": "EAS",
+    "player": (1, 0),
+    "pucks": [(1, 1)],
+    "obstacles": [],
+    "goals": [(2, 1)],
+    "cost_matrix": ((1, 1, 1),
+                    (1, 1, 1),
+                    (1, 1, 1)),
+    "cost_matrix_1": ((2, 1, 1),
+                      (0, 2, 3),
+                      (2, 1, 1))
+}
+
+test3 = {
+    "player": (),
+    "pucks": [],
+    "obstacles": [],
+    "goals": [],
+    "cost_matrix": ()
+}
+
+test4 = {
+    "player": (),
+    "pucks": [],
+    "obstacles": [],
+    "goals": [],
+    "cost_matrix": ()
+}
+
+test5 = {
+    "player": (),
+    "pucks": [],
+    "obstacles": [],
+    "goals": [],
+    "cost_matrix": ()
+}

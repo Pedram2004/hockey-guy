80cbed986f3605ae552e90dfbd8131d514b3990e
Magnesium <mehdighorashi83@gmail.com>
2024-11-10 18:37:57 +0330

    added heuristic function

--- hockey/search_tree_node.py
+++ hockey/search_tree_node.py
@@ -1,5 +1,5 @@
 from playground import PlayGround
-
+from munkres import Munkres
 
 class Node:
     def __init__(self, state: PlayGround, parent: "Node" = None, direction: str = "", cost: int = 0):
@@ -68,3 +68,31 @@ class Node:
                      direction=direction,
                      cost=self.__cost_from_root + move_s_cost)
             )
+
+    def __manhattan(self, a: tuple[int, int], b: tuple[int, int]) -> int:
+        return abs(a[0] - b[0]) + abs(a[1] - b[1])
+    
+    def heuristic_function(self) -> int:
+        h = 0 # heuristic value
+
+        m = Munkres()
+        cost_matrix = [[self.__manhattan(puck, goal) for goal in self.__state.goals] for puck in self.__state.pucks]
+        indexes = sorted(m.compute(cost_matrix))
+
+        for row, column in indexes:
+            h += cost_matrix[row][column]
+        
+        pucks = self.__state.pucks.copy()
+        current = self.__state.player
+        while len(pucks) > 1:
+            closest_puck = min(pucks, key=lambda x: self.__manhattan(current, x[0]))
+            h += self.__manhattan(current, closest_puck[0])
+            current = self.__state.goals[indexes[self.__state.pucks.index(closest_puck)][1]] # goal of the puck
+            pucks.remove(closest_puck)
+        else:
+            h += self.__manhattan(current, pucks[0][0])
+        
+        return h
+        
+        
+        
